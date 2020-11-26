d5db34e0b31859bc7f7e2907374edb489d3e1bae
catherineee999 <j297430129@gmail.com>
2020-11-27 00:27:05 +0800

    upload

---
+++ AFK_Arena/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AFK_Arena
+//
+//  Created by yangchienying on 2020/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
